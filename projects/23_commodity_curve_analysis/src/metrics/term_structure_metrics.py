96708b3744bcea3e4b4b4295f6386a43d6fbeacf
ayetahscomet <ayetahscomet@gmail.com>
2025-11-12 14:24:09 +0000

    "recent_updates"

--- projects/23_commodity_curve_analysis/src/metrics/term_structure_metrics.py
+++ projects/23_commodity_curve_analysis/src/metrics/term_structure_metrics.py
@@ -3,7 +3,8 @@
 import numpy as np
 import pandas as pd
 import os
+from pathlib import Path
 
-os.getcwd()
-LOCATION = "workspaces"
-os.chdir(f"/{LOCATION}/quant-strat-lab/projects/23_commodities_curve_analysis/src")
+LOCATION = Path("workspaces")  # no leading slash
+path = LOCATION / "quant-strat-lab" / "projects" / "23_commodities_curve_analysis" / "src"
+os.chdir(path)
